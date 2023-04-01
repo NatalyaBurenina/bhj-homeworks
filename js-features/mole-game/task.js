e4077c23cca2f1829833986835724da0bf207287
Наталья Буренина <bur_nat_nev@mail.ru>
2023-04-01 22:19:36 +0300

    Убей кротов 2

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,33 +1,25 @@
 // Задача 3.
 
-let getHole = index => document.getElementById(`hole${index}`);
-let deadMole = document.getElementById('dead');
-let lostMole = document.getElementById('lost');
-let victory = parseInt(deadMole.textContent);
-let defeat = parseInt(lostMole.textContent);
+const getHole = index => document.getElementById(`hole${index}`);
+const dead = document.getElementById("dead")
+const lost = document.getElementById("lost")
 
-function checkGame(deadMole, lostMole) {
-    console.log(deadMole, lostMole);
-    if (deadMole === 10) {
-        alert("Вы победили!");
-        location.reload();
-    } else if (lostMole === 5) {
-        alert("Вы приграли!");
-        location.reload();
-    };
-};
-
-for (let i = 1; i <= 9; i++) {
-    getHole(i).onclick = function () {
-        if (this.className.includes('hole_has-mole')) {
-            victory += 1;
-            deadMole.textContent = victory;
+for(i = 1; i <= 9; i++){
+    const play = document.getElementById(`hole${i}`)    
+    play.onclick = function(){
+        if(play.className.includes('hole_has-mole')){
+            dead.textContent = +dead.textContent + 1
         } else {
-            defeat += 1;
-            lostMole.textContent = defeat;
-        };
-        checkGame(victory, defeat);
-    };
-};
-
-
+            lost.textContent = +lost.textContent + 1
+        }
+        if(+lost.textContent === 5){
+            alert("Вы проиграли!")
+            dead.textContent = 0
+            lost.textContent = 0
+        } else if(+dead.textContent === 10) {
+            alert("Победа!")
+            dead.textContent = 0
+            lost.textContent = 0
+        }
+    }     
+}
