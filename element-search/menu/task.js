09157b236623c3c07d7fb2c9b87ca610b352436b
Наталья Буренина <bur_nat_nev@mail.ru>
2023-04-05 19:43:00 +0300

    Навигационное меню2

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -3,7 +3,7 @@ let menu = Array.from(document.querySelectorAll('.menu__link'));
 for (let link of menu) {
 	link.onclick = function () {
 		let parent = link.parentElement;
-		if (parent.querySelector('.menu_sub').className = 'menu menu_sub') {
+		if (parent.querySelector('.menu_sub').className === 'menu menu_sub') {
 			parent.querySelector('.menu_sub').className = 'menu menu_sub menu_active';
 		} else {
 			parent.querySelector('.menu_sub').className = 'menu menu_sub'
