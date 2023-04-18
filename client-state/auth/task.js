f47ec89edb375ef0416ea93404e9be525bdf007c
Наталья Буренина <bur_nat_nev@mail.ru>
2023-04-18 23:26:28 +0300

    Черновик3

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -3,23 +3,36 @@ const welcome = document.getElementById('welcome');
 const signinBtn = document.getElementById('signin__btn');
 const userId = document.getElementById('user_id');
 
+function authoriz(id) {
+	formDiv.classList.toggle('signin_active');
+	welcome.classList.toggle('welcome_active');
+	userId.innerText = id;
+}
+
 form.addEventListener('submit', e => {
-	event.preventDefault();
-	let form = document.getElementById('signin__form');
+	e.preventDefault();
 	let formData = new FormData(form);
 
 	let xhr = new XMLHttpRequest();
 	xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
 	xhr.send(formData);
+
+
+	xhr.addEventListener('load', () => {
+
+		if (xhr.response.success) {
+			let auth = xhr.response;
+			localStorage.setItem('user_id', auth.user_id);
+			authoriz(auth.user_id)
+		} else {
+			alert('Неверный логин/пароль');
+		}
+	});
 });
 
-xhr.addEventListener('load', () => {
-	if (xhr.response.success) {
-		window.onload.localStorage.setItem('user_id');
-		formDiv.classList.remove('signin_active');
-		welcome.classList.add('welcome_active');
-		userId.innerText = response.user_id;
-	} else {
-		alert('Неверный логин/пароль');
+window.onload = function() {
+	const localeId = localStorage.getItem('user_id');
+	if (userId.textContent == localeId) {
+		authoriz(localeId);
 	}
-});
+};
